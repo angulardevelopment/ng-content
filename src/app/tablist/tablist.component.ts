42c9b8f0054f782ef0882e48a97ff38f005ff003
Sunny Gupta <sunny.gupta@globallogic.com>
2022-08-28 22:45:54 +0530

    refactor

--- src/app/tablist/tablist.component.ts
+++ src/app/tablist/tablist.component.ts
@@ -14,7 +14,14 @@ import { TabComponent } from '../tab/tab.component';
 export class TablistComponent {
   //  return directives
   // @ContentChildren(TabComponent) tabList: QueryList<TabComponent>;
+  // @ContentChild(‘contentParagraph’) paragraph: ElementRef;
 
+// @ViewChildren(‘input’) inputs: QueryList<ElementRef>;
+
+// onClick(index) {
+// this.inputs.toArray()[index].nativeElement.focus();
+// }
+  // <ng-content></ng-content>
   // ngAfterContentInit() {
     // to run methods of tab component
   //   this.tabList.toArray()[0].printTitle();
