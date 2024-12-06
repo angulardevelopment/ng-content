edfdfc7ff3dea64cf8d176339c19598fa147806b
Gupta, Sunny <sunnyvales789@gmail.com>
2024-12-06 09:08:50 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Home1Component } from './home1/home1.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'HomeComponent', component: Home1Component}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
